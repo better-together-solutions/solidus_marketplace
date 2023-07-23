76f5b01fc225095ceac1ace7fa3c6ab725009192
Robert Smith <rob@bettertogethersolutions.com>
2023-07-23 19:09:31 -0230

    Make engine initializer Zeitwerk compatible

--- lib/solidus_marketplace/engine.rb
+++ lib/solidus_marketplace/engine.rb
@@ -18,36 +18,42 @@ module SolidusMarketplace
     end
 
     initializer 'solidus_marketplace.custom_splitters', after: 'spree.register.stock_splitters' do |app|
-      app.config.spree.stock_splitters << Spree::Stock::Splitter::Marketplace
+      Rails.application.config.to_prepare do
+        app.config.spree.stock_splitters << ::Spree::Stock::Splitter::Marketplace
+      end
     end
 
     initializer 'solidus_marketplace.preferences', before: :load_config_initializers  do |app|
-      SolidusMarketplace::Config = SolidusMarketplace::Configuration.new
-      Spree::PermittedAttributes.singleton_class.prepend(SolidusMarketplace::PermittedAttributes)
-      Spree::Config.roles.assign_permissions :supplier_admin, ['Spree::PermissionSets::Supplier::AdminAbility']
-      Spree::Config.roles.assign_permissions :supplier_staff, ['Spree::PermissionSets::Supplier::StaffAbility', 'Spree::PermissionSets::OrderManagement']
+      Rails.application.config.to_prepare do
+        SolidusMarketplace::Config = SolidusMarketplace::Configuration.new
+        ::Spree::PermittedAttributes.singleton_class.prepend(SolidusMarketplace::PermittedAttributes)
+        ::Spree::Config.roles.assign_permissions :supplier_admin, ['::Spree::PermissionSets::Supplier::AdminAbility']
+        ::Spree::Config.roles.assign_permissions :supplier_staff, ['::Spree::PermissionSets::Supplier::StaffAbility', '::Spree::PermissionSets::OrderManagement']
+      end
     end
-
+      
     initializer 'solidus_marketplace' do
-      next unless ::Spree::Backend::Config.respond_to?(:menu_items)
-      ::Spree::Backend::Config.configure do |config|
-        config.menu_items << Spree::BackendConfiguration::MenuItem.new(
-          [:stock_locations],
-          'globe',
-          condition: -> { can?(:index, ::Spree::StockLocation) },
-        )
+      Rails.application.config.to_prepare do
+        next unless ::Spree::Backend::Config.respond_to?(:menu_items)
+        ::Spree::Backend::Config.configure do |config|
+          config.menu_items << ::Spree::BackendConfiguration::MenuItem.new(
+            [:stock_locations],
+            'globe',
+            condition: -> { can?(:index, ::Spree::StockLocation) },
+          )
 
-        config.menu_items << Spree::BackendConfiguration::MenuItem.new(
-          [:suppliers],
-          'home',
-          condition: -> { can?(:index, ::Spree::Supplier) },
-        )
+          config.menu_items << ::Spree::BackendConfiguration::MenuItem.new(
+            [:suppliers],
+            'home',
+            condition: -> { can?(:index, ::Spree::Supplier) },
+          )
 
-        config.menu_items << Spree::BackendConfiguration::MenuItem.new(
-          [:shipments],
-          'plane',
-          condition: -> { can?(:index, ::Spree::Shipment) },
-        )
+          config.menu_items << ::Spree::BackendConfiguration::MenuItem.new(
+            [:shipments],
+            'plane',
+            condition: -> { can?(:index, ::Spree::Shipment) },
+          )
+        end
       end
     end
   end
